76dcb6b0cd8b1b93737d19b837a7a3e815fffdd8
Devyansh0004 <devyansh0004@gmail.com>
2026-09-06 16:42:07 +0530

    Time: 21 ms (91.79%), Space: 16.1 MB (96.62%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,40 +1,26 @@
-const static auto fast_io = [](){ cin.tie(nullptr); ios::sync_with_stdio(false); return 0; }();
-
 class Solution {
 public:
+    void consumeIsland(int i,int j,int m,int n,vector<vector<char>>& grid){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!='1') return;
+
+        grid[i][j]='0';//most imp mark it is visited
+
+        consumeIsland(i-1,j,m,n,grid);
+        consumeIsland(i+1,j,m,n,grid);
+        consumeIsland(i,j-1,m,n,grid);
+        consumeIsland(i,j+1,m,n,grid);
+    }
     int numIslands(vector<vector<char>>& grid) {
-        //BFS traversal most intuitive approach
+        //Dfs approach :popular one
         int m=grid.size();
         int n=grid[0].size();
+
         int islands=0;
-
-        int dr[]={0,0,1,-1};
-        int dc[]={1,-1,0,0};
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
                     islands++;
-
-                    queue<pair<int,int>>q;
-                    q.push({i,j});
-                    grid[i][j]='0';
-                    //visited marked in grid itself
-                    while(!q.empty()){
-                        auto [r,c]=q.front();
-                        q.pop();
-                        for(int d=0;d<4;d++){
-                            int nr=r+dr[d];
-                            int nc=c+dc[d];
-
-                            if(nr>=0 && nr<m && nc>=0 && nc<n){
-                                if(grid[nr][nc]=='1'){
-                                    q.push({nr,nc});
-                                    grid[nr][nc]='0';
-                                }
-                            }
-                        }
-                    }
+                    consumeIsland(i,j,m,n,grid);
                 }
             }
         }
