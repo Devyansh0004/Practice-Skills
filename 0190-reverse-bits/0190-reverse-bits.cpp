4a62fec565fdba666d56302510707f35b49c6bcf
Devyansh0004 <devyansh0004@gmail.com>
2026-09-09 19:49:53 +0530

    Time: 0 ms (100%), Space: 8.4 MB (14.36%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,32 +1,12 @@
 class Solution {
-public: 
-    int fastExpo(long long x,long long n){
-        long long ans=1;
-        while(n>0){
-            if(n&1){
-                ans=ans*x;
-            }
-            x=x*x;
-            n>>=1;
+public:
+    int reverseBits(int n) {
+        int ans=0;
+        for(int i=0;i<32;i++){
+            ans=ans<<1;//first bit is slowly moving towards msb 
+            ans=ans|(n&1);//doing or will make last bit of ans 1 or 0 acc to bit of n
+            n>>=1;//finding last bit of n and first bit of n will come at last
         }
         return ans;
-    }
-    int reverseBits(int n) {
-        //can use fastExpo for better TC
-        vector<int>bit(32,0);
-        int temp=n;
-        int i=0;
-        while(temp>0){
-            bit[i]=temp&1;
-            temp>>=1;
-            i++;
-        }
-        long long ans=0;
-        for(int i=31;i>=0;i--){
-            if(bit[i]==1){
-                ans=ans+fastExpo(2,31-i);
-            }
-        }
-        return (int)ans;
     }
 };
